116b65ddee6340c9ad5b2d0734e689187f5f3dbc
Kartik <kartiktornadus098@gmail.com>
2024-04-03 16:45:43 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const input = document.querySelector(".input-box");
+const button = document.querySelector(".add-button");
+const todolist = document.querySelector(".todo-list");
+
+button.addEventListener("click", addTodo);
+todolist.addEventListener("click", deleteTodo)
+
+function addTodo(event){
+    event.preventDefault()
+
+    const todoDiv = document.createElement("div");//creates element
+    todoDiv.classList.add("todo-container");//makes a new class and adds it to the array
+
+    const todoItem = document.createElement("li");
+    todoItem.classList.add("todo-item");
+    todoItem.innerText = input.value;
+    todoDiv.appendChild(todoItem)//pushes item to the main div
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-btn");
+    deleteButton.innerHTML='<i class="fa-sharp fa-solid fa-trash"></i>';
+    todoDiv.appendChild(deleteButton);
+
+    const completeButton = document.createElement("button");
+    completeButton.classList.add("complete-btn");
+    completeButton.innerHTML='<i class="fa-solid fa-check"></i>';
+    todoDiv.appendChild(completeButton);
+
+
+    todolist.appendChild(todoDiv);
+    input.value ="";
+}
+
+function deleteTodo(event){
+    const item = event.target;
+    if(item.classList[0]=== "delete-btn"){
+        const delItem = item.parentElement;
+        delItem.remove();
+    }
+    if(item.classList[0] === "complete-btn"){
+        const delItem = item.parentElement;
+        delItem.classList.toggle("completed");
+    }
+}
